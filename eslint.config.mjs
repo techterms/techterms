f70940e14d397a2263959715625c9febdece8069
Razvan Muntian <razvanmuntian@gmail.com>
2025-06-16 17:13:48 +0100

    Migrate .eslintrc.json to eslint.config.mjs and formatting - Work In Progress

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import astroPlugin from 'eslint-plugin-astro';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['.astro/**', 'dist/**'],
+  },
+  eslint.configs.recommended,
+  ...astroPlugin.configs['flat/recommended'],
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroPlugin.parser,
+      parserOptions: {
+        parser: tsparser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+    plugins: {
+      astro: astroPlugin,
+    },
+    rules: {
+      ...astroPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  prettier,
+];
