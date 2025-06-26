e792191196d2e27b2241137e5f901e4e072e0e60
Lapis Rowe <lapisrowe@gmail.com>
2025-06-26 20:25:40 +0530

    Add general software engineering terms with definitions and references (#47)
    - Monolithic Arch
    - CI/CD
    - OOP
    - SOLID
    - DRY
    - KISS
    - YAGNI
    - Technical Debt
    - Refactoring
    - Unit Testing
    - Code Review
    - MVC

--- src/models/Categories.ts
+++ src/models/Categories.ts
@@ -4,4 +4,5 @@ export enum Categories {
   'DevOps' = 'DevOps',
   'SoftwareArchitecture' = 'Software Architecture',
   'Database' = 'Database',
+  'SoftwareEngineering' = 'Software Engineering',
 }
