e34f573f0ede0e8691ab32ae9d0d4bf8f304ca2f
Razvan Muntian <144447396+razvanmtn@users.noreply.github.com>
2025-07-03 19:20:32 +0300

    Add @astrojs/sitemap package (#68)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://techterms.io',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
