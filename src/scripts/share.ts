f4074072c662dbbda899782e6da44c6844db9d38
Razvan Muntian <razvanmuntian@gmail.com>
2025-07-09 17:16:52 +0300

    Add bluesky share button and fix share svg import

--- src/scripts/share.ts
+++ src/scripts/share.ts
@@ -1,4 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+const BASE_URL = 'https://techterms.io';
+
+function getText(definitionTerm: string, definitionExplanation: string) {
+  return encodeURIComponent(`${definitionTerm} = ${definitionExplanation}`);
+}
+
+function getUrl(definitionId: string) {
+  return encodeURIComponent(`${BASE_URL}/t/${definitionId}`);
+}
+
+function getTextWithUrl(
+  definitionTerm: string,
+  definitionExplanation: string,
+  definitionId: string
+) {
+  return encodeURIComponent(
+    `${definitionTerm} = ${definitionExplanation} ${BASE_URL}/t/${definitionId}`
+  );
+}
+
+function initTwitterShare() {
   const buttons = document.querySelectorAll('[data-type="share-on-x-button"]');
   if (!buttons || buttons.length === 0) {
     return;
@@ -14,11 +34,35 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
       }
 
-      const text = `${definitionTerm} = ${definitionExplanation}\n\n`;
-      const url = `https://techterms.io/t/${definitionId}`;
-
-      const intentUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`;
+      const intentUrl = `https://twitter.com/intent/tweet?text=${getText(definitionTerm, definitionExplanation)}&url=${getUrl(definitionId)}`;
       window.open(intentUrl, '_blank');
     });
   });
+}
+
+function initBlueSkyShare() {
+  const buttons = document.querySelectorAll('[data-type="share-on-bsky-button"]');
+  if (!buttons || buttons.length === 0) {
+    return;
+  }
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const definitionId = button.getAttribute('data-definition-id')?.trim();
+      const definitionTerm = button.getAttribute('data-definition-term')?.trim();
+      const definitionExplanation = button.getAttribute('data-definition-explanation')?.trim();
+
+      if (!definitionId || !definitionTerm || !definitionExplanation) {
+        return;
+      }
+
+      const intentUrl = `https://bsky.app/intent/compose?text=${getTextWithUrl(definitionTerm, definitionExplanation, definitionId)}`;
+      window.open(intentUrl, '_blank');
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initTwitterShare();
+  initBlueSkyShare();
 });
